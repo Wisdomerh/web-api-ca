a39c25e6174c0d771b35752ceb13f0a13c01207e
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2024-12-31 21:03:46 +0000

    add error handling and getMovies from tmdb

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -1,5 +1,21 @@
 import fetch from 'node-fetch';
 
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+
+const handleErrors = async (response) => {
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message || 'TMDB API error');
+    }
+    return response.json();
+};
+export const getMovies = async () => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    );
+    return handleErrors(response);
+};
+
 export const getUpcomingMovies = async () => {
     try {
         const response = await fetch(
