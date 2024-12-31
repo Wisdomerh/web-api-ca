10206bc020e6d8b136fc1e7d6826515cfa4a7373
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2024-12-31 22:39:37 +0000

    Add movie endpoints

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -2,10 +2,10 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 import {
-    getUpcomingMovies
-  } from '../tmdb-api';
-import {
-getGenres
+    getMovies, getMovie, getGenres, getMovieImages, getMovieReviews,
+    getUpcomingMovies, getNowPlayingMovies, getTopRatedMovies,
+    getPopularMovies, getTrendingMovies, getMovieRecommendations,
+    getMovieCredits, getPersonDetails, getPersonMovieCredits
 } from '../tmdb-api';
   
 const router = express.Router();
@@ -32,25 +32,88 @@ router.get('/', asyncHandler(async (req, res) => {
 }));
 
 
+// Get discover movies
+router.get('/tmdb/discover', asyncHandler(async (req, res) => {
+    const movies = await getMovies();
+    res.status(200).json(movies);
+}));
+
 // Get movie details
-router.get('/:id', asyncHandler(async (req, res) => {
+router.get('/tmdb/movie/:id', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
-    const movie = await movieModel.findByMovieDBId(id);
-    if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
-    }
+    const movie = await getMovie(id);
+    res.status(200).json(movie);
 }));
 
-router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
-    const upcomingMovies = await getUpcomingMovies();
-    res.status(200).json(upcomingMovies);
-}));
-
+// Get genres
 router.get('/tmdb/genres', asyncHandler(async (req, res) => {
     const genres = await getGenres();
     res.status(200).json(genres);
+}));
+
+// Get movie images
+router.get('/tmdb/movie/:id/images', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const images = await getMovieImages(id);
+    res.status(200).json(images);
+}));
+
+// Get upcoming movies
+router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
+    const movies = await getUpcomingMovies();
+    res.status(200).json(movies);
+}));
+
+// Get now playing movies
+router.get('/tmdb/now-playing', asyncHandler(async (req, res) => {
+    const movies = await getNowPlayingMovies();
+    res.status(200).json(movies);
+}));
+
+// Get top rated movies
+router.get('/tmdb/top-rated', asyncHandler(async (req, res) => {
+    const movies = await getTopRatedMovies();
+    res.status(200).json(movies);
+}));
+
+// Get popular movies
+router.get('/tmdb/popular', asyncHandler(async (req, res) => {
+    const movies = await getPopularMovies();
+    res.status(200).json(movies);
+}));
+
+// Get trending movies
+router.get('/tmdb/trending', asyncHandler(async (req, res) => {
+    const movies = await getTrendingMovies();
+    res.status(200).json(movies);
+}));
+
+// Get movie recommendations
+router.get('/tmdb/movie/:id/recommendations', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const recommendations = await getMovieRecommendations(id);
+    res.status(200).json(recommendations);
+}));
+
+// Get movie credits
+router.get('/tmdb/movie/:id/credits', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const credits = await getMovieCredits(id);
+    res.status(200).json(credits);
+}));
+
+// Get person details
+router.get('/tmdb/person/:id', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const person = await getPersonDetails(id);
+    res.status(200).json(person);
+}));
+
+// Get person movie credits
+router.get('/tmdb/person/:id/movie-credits', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const credits = await getPersonMovieCredits(id);
+    res.status(200).json(credits);
 }));
 
 export default router;
