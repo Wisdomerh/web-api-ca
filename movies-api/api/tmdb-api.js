a6f290d7704af70e8916d771d7e34a867f0edd2b
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2024-12-31 22:07:43 +0000

    add complete api calls to tmdb

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -37,3 +37,79 @@ export const getMovieImages = async (id) => {
     return handleErrors(response);
 };
 
+export const getMovieImages = async (id) => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/${id}/images?api_key=${process.env.TMDB_KEY}`
+    );
+    return handleErrors(response);
+};
+
+export const getMovieReviews = async (id) => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/${id}/reviews?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+    return handleErrors(response);
+};
+
+export const getUpcomingMovies = async () => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+    return handleErrors(response);
+};
+
+export const getNowPlayingMovies = async () => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/now_playing?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+    return handleErrors(response);
+};
+
+export const getTopRatedMovies = async () => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/top_rated?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+    return handleErrors(response);
+};
+
+export const getPopularMovies = async () => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+    return handleErrors(response);
+};
+
+export const getTrendingMovies = async () => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/trending/movie/week?api_key=${process.env.TMDB_KEY}`
+    );
+    return handleErrors(response);
+};
+
+export const getMovieRecommendations = async (id) => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/${id}/recommendations?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+    return handleErrors(response);
+};
+
+export const getMovieCredits = async (id) => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/${id}/credits?api_key=${process.env.TMDB_KEY}&language=en-US`
+    );
+    return handleErrors(response);
+};
+
+export const getPersonDetails = async (id) => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/person/${id}?api_key=${process.env.TMDB_KEY}&language=en-US`
+    );
+    return handleErrors(response);
+};
+
+export const getPersonMovieCredits = async (id) => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/person/${id}/movie_credits?api_key=${process.env.TMDB_KEY}&language=en-US`
+    );
+    return handleErrors(response);
+};
