78502743ee3b9d1eff079fecd98e5eaa4957ed4b
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2024-12-31 21:14:05 +0000

    add more api call funtions

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -16,34 +16,24 @@ export const getMovies = async () => {
     return handleErrors(response);
 };
 
-export const getUpcomingMovies = async () => {
-    try {
-        const response = await fetch(
-            `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
-        );
-
-        if (!response.ok) {
-            throw new Error(response.json().message);
-        }
-
-        return await response.json();
-    } catch (error) {
-        throw error;
-    }
+export const getMovie = async (id) => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/${id}?api_key=${process.env.TMDB_KEY}`
+    );
+    return handleErrors(response);
 };
 
 export const getGenres = async () => {
-    try {
-        const response = await fetch(
-             `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US`
-        );
-
-        if (!response.ok) {
-            throw new Error(response.json().message);
-        }
-
-        return await response.json();
-    } catch (error) {
-        throw error;
-    }
+    const response = await fetch(
+        `${tmdbBaseUrl}/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US`
+    );
+    return handleErrors(response);
 };
+
+export const getMovieImages = async (id) => {
+    const response = await fetch(
+        `${tmdbBaseUrl}/movie/${id}/images?api_key=${process.env.TMDB_KEY}`
+    );
+    return handleErrors(response);
+};
+
