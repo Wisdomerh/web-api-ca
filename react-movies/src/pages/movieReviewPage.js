3c631da0cdfdc6e416a37dd2b09b0f016b6b7d1d
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2025-01-03 23:20:30 +0000

    Update movieReviewPage.js

--- react-movies/src/pages/movieReviewPage.js
+++ react-movies/src/pages/movieReviewPage.js
@@ -3,10 +3,19 @@ import { useLocation } from "react-router-dom";
 import PageTemplate from "../components/templateMoviePage";
 import MovieReview from "../components/movieReview";
 
-const MovieReviewPage = (props) => {
-  let location = useLocation();
-  const {movie, review} = location.state;
-  
+const MovieReviewPage = () => {
+  const location = useLocation();
+  const { movie, review } = location.state;
+
+  // Add a check for review data
+  if (!review) {
+    return (
+      <PageTemplate movie={movie}>
+        <p>No review found.</p>
+      </PageTemplate>
+    );
+  }
+
   return (
     <PageTemplate movie={movie}>
       <MovieReview review={review} />
